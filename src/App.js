2bd7fc904c5353bce240253f50f5390282373ba4
Aditya <adityamore360123@gmail.com>
2021-09-17 14:08:48 +0530

    initial project structuring & initialization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import "./App.css";
+import About from './components/About/About';
+import Dashboard from './components/Dashboard/Dashboard';
+import Events from './components/Events/Events';
+import Team from './components/Team/Team';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <Router>
+                <div>
+                    <Route path='/' exact component={Dashboard} />
+                    <Route path='/home' exact component={Dashboard} />
+                    <Route path='/about' exact component={About} />
+                    <Route path='/events' exact component={Events} />
+                    <Route path='/team' exact component={Team} />
+                </div>
+            </Router>
+        </div>
+    )
 }
 
-export default App;
+export default App
